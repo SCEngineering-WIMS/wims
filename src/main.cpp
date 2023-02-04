3c05363476ebc77cc9f717122ca8460722539ae4
sccashma <39361797+sccashma@users.noreply.github.com>
2023-02-04 20:30:31 +0800

    added crow (#2)

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
-int main(int argc, char const *argv[])
+
+#include <crow.h>
+
+int main()
 {
+    crow::SimpleApp app; //define your crow application
+
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        return "Hello World";
+    });
+
+    //set the port, set the app to run on multiple threads, and run the app
+    app.bindaddr("192.168.50.110").port(8080).multithreaded().run();
+
     return 0;
 }
